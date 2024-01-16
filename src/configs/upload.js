e301f2ced3d57e1b8f291903547fb798c66e88ea
vctrhugoop <vhdestroy@hotmail.com>
2024-01-16 20:30:08 -0300

    ✨ feat: create upload image

---
+++ src/configs/upload.js
@@ -0,0 +1,18 @@
+import { path } from 'path';
+import multer from 'multer';
+import crypto from 'crypto';
+
+export const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp');
+export const UPLOAD_FOLDER = path.resolve(__dirname, 'upload');
+
+export const MULTER = {
+  Storage: multer.diskStorage({
+    destination: TMP_FOLDER,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
