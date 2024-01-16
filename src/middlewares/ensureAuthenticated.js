66b8b4b81067e2504cef0692dff1a93ae1733891
vctrhugoop <vhdestroy@hotmail.com>
2024-01-16 20:02:47 -0300

    ✨ feat:

---
+++ src/middlewares/ensureAuthenticated.js
@@ -0,0 +1,25 @@
+import { verify } from 'jsonwebtoken';
+import AppError from '../utils/AppError.js';
+import authConfig from '../configs/auth.js';
+
+export function ensureAuthenticated(request, response, next) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT Token não informado', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(token, authConfig.jwt.secret);
+
+    request.user = {
+      id: Number(user_id),
+    };
+
+    return next();
+  } catch {
+    throw new AppError('JWT Token inválido', 401);
+  }
+}
